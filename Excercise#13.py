712cadf96b2ae3fdf8296c25d0b02f24a71b988d
Pisit Chingulprasan <66056059@kmtil.ac.th>
2023-10-01 12:41:37 +0700

    Add Ecercise#13

---
+++ Excercise#13.py
@@ -0,0 +1,65 @@
+import random
+def calculateSum(list_data):
+    total = 0
+    if list_data.__len__() == 0:
+        return 0
+    else:
+        for i in list_data:
+            total += i
+    return total
+
+
+def calculateProduct(list_data):
+    total = 1
+    if list_data.__len__() == 0:
+        return 1
+    else:
+        for i in list_data:
+            total *= i
+    return total
+
+
+def average(list_data):
+    total = 0
+    avg = 0
+    if list_data == []:
+        return 'Nan'
+
+    elif list_data.__len__() == 0:
+        return 0
+    else:
+        for i in list_data:
+            total += i
+    avg = total / list_data.__len__()
+    return avg
+
+
+def median(list_data):
+    index = 0
+    list_data.sort()
+    if list_data == []:
+        return 'None'
+    elif list_data.__len__() % 2 == 0:
+        index = list_data.__len__()//2
+        return int(list_data[index])
+    else:
+        index = index = list_data.__len__()//2
+        return int(list_data[index])
+
+
+if __name__ == '__main__':
+    # print(calculateSum([]) == 0)
+    # print(calculateSum([2, 4, 6, 8, 10]) == 30)
+    # print(calculateSum([1, 3, 5, 7, 9]) == 25)
+    # print(calculateProduct([]) == 1)
+    # print(calculateProduct([2, 4, 6, 8, 10]) == 3840)
+    # print(average([1,2,3]) == 2)
+    # print(average([1,2,3,1,2,3,1,2,3]) == 2)
+    # print(average([12,20,37]) == 23)
+    # print(average([0,0,0,0]) == 0)
+    # print(average([]))
+    print(median([]) == 'None')
+    print(median([1,2,3]) == 2)
+    print(median([3,7,10,4,19,6,5,2,8]) == 5.5)
+    # print(median([3,7,10,4,19,6,5,2]) == 6)
+
