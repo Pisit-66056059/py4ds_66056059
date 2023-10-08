99016860fcb6c8b2c738678469f6848cfbf2b0e8
Pisit Chingulprasan <66056059@kmtil.ac.th>
2023-10-08 12:37:20 +0700

    Fix ex06

--- src/ex06.py
+++ src/ex06.py
@@ -1,9 +1,9 @@
 """
-Execise 6
+Exercise 6
 """
 
 
-def ordinal_suffix(param):
+def ordinal_suffix(num):
     """
     Return the ordinal suffix for a given number.
 
@@ -13,5 +13,17 @@ def ordinal_suffix(param):
     Returns:
         str: The ordinal suffix corresponding to the given number.
     """
-    # TODO : complete this
-    pass
+    # Fix : complete this
+    if (num >= 10) & (num < 20):
+        return f'{num}th'
+    else:
+        if num % 10 == 0:
+            return f'{num}th'
+        elif num % 10 == 1:
+            return f'{num}st'
+        elif num % 10 == 2:
+            return f'{num}nd'
+        elif num % 10 == 3:
+            return f'{num}rd'
+        else:
+            return f'{num}th'
