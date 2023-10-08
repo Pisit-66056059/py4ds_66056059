15c5df5881bd896e2a28284492e04ce1076abdb1
Pisit Chingulprasan <66056059@kmtil.ac.th>
2023-10-08 12:37:35 +0700

    Fix ex09

--- src/ex09.py
+++ src/ex09.py
@@ -1,5 +1,5 @@
 """
-Execise 9
+Exercise 9
 """
 
 
@@ -19,4 +19,7 @@ def get_chess_square_color(col, row):
         str: The color of the chess square, either 'white' or 'black'.
     """
     # TODO : complete this
-    pass
+    if (col + row) %2 == 0:
+        return 'White'
+    else:
+        return 'Black'
