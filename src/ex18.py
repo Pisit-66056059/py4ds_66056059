e48ec5eedfbd77c9f1f67a499ad2b953b77268b6
Pisit Chingulprasan <66056059@kmitl.ac.th>
2023-10-10 20:45:37 +0700

    Fix ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,13 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(cups, unit_per_cup):
+    total = 0
+    if cups > 0:
+        if cups != 8:
+            total = (cups - (cups // 8)) * unit_per_cup
+        else:
+            total = cups * unit_per_cup
+    else:
+        return 0
+    return total
