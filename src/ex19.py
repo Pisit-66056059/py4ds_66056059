05ee44369e0c6e1adc8f96761640a2a4fe7a90e0
Pisit Chingulprasan <66056059@kmitl.ac.th>
2023-10-14 19:37:59 +0700

    Fix ex19

--- src/ex19.py
+++ src/ex19.py
@@ -11,6 +11,18 @@ SPECIAL = '~!@#$%^&*()_+'                       # 13
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
-    # TODO : complete this
-    pass
+def gen_password(length):
+    # Fix : complete this
+    if length < 12:
+        length = 12
+    password = []
+    password.append(LOWER_LETTERS[random.randint(0,25)])
+    password.append(UPPER_LETTERS[random.randint(0,25)])
+    password.append(NUMBERS[random.randint(0,10)])
+    password.append(SPECIAL[random.randint(0,13)])
+    print(password)
+    while len(password) < length:
+        password.append(ALL_CHARS[random.randint(0,74)])
+    return ''.join(password)
+
+
